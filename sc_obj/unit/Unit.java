7eb67d365c22d4ed13ad71bd2c6dc39453a44326
bwmelon97 <bwmelon97@naver.com>
2020-09-27 05:33:32 +0000

    Update class Unit

--- sc_obj/unit/Unit.java
+++ sc_obj/unit/Unit.java
@@ -1,5 +1,22 @@
 package sc_obj.unit;
 
 public class Unit {
-    
+    private final int population;
+    private final int moveSpeed;
+
+    public Unit(final int population, final int moveSpeed) {
+        this.population = population;
+        this.moveSpeed = moveSpeed;
+    }
+
+    public int getPopulation() { return population; }
+    public int getMoveSpeed() { return moveSpeed; }
+
+    public void move() {
+
+    }
+
+    public void stop() {
+        
+    }
 }
